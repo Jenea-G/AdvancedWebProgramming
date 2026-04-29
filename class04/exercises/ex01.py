e636d4fec3f44a2dade1cb050aeb2f2760a2d8a9
Jenea-G <eugeniagurulea@gmail.com>
2026-04-29 10:37:37 -0400

    add exercise01 correction

--- class04/exercises/ex01.py
+++ class04/exercises/ex01.py
@@ -1,23 +1,27 @@
 class Employee:
     bonus = 0.25
     base_salary = 500
+    company_name = "Creators"
 
     def __init__ (self, name, sales_count):
         self.name = name
         self.sales_count = sales_count
 
-    def salary(self):
-        base_salary = Employee.base_salary
-        bonus = Employee.bonus
-        
+    def calc_salary(self):
+        salary = self.base_salary
+
         if self.sales_count > 10:
-            salary = base_salary + self.sales_count * bonus
-            return salary
-        else:
-            return base_salary
+            salary = salary + self.sales_count * self.bonus
+        return salary
         
 employee1 = Employee("Kelly", 12)
 employee2 = Employee("Bob", 6)
 
-print(employee1.salary())
-print(employee2.salary())
+print(employee1.calc_salary())
+print(employee2.calc_salary())
+
+print("new base salary 1000")
+Employee.base_salary = 1000
+
+print(employee1.calc_salary())
+print(employee2.calc_salary())
