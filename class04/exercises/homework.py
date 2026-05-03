a7cf992423015d24704572a0eb98dd68c54fd375
Jenea-G <eugeniagurulea@gmail.com>
2026-05-03 16:37:42 -0400

    add ex2 for class4 homework

--- class04/exercises/homework.py
+++ class04/exercises/homework.py
@@ -20,3 +20,28 @@ book3 = Book("World Without End", "Ken Follett")
 print(Book.book_counter)
 
 # Exercise 2 -- Create a class called Student.
+print("==== Exercise 2 ====")
+class Student:
+    school_name = "Vanier College"
+    student_count = 0
+
+    def __init__ (self, name, program, grade):
+        self.name = name
+        self.program = program
+        self.grade = grade
+        self.increment_count()
+
+    def increment_count(self):
+        Student.student_count += 1
+
+    def display_info(self):
+        print(f"{self.name} studies {self.program} at {self.school_name}. Grade: {self.grade}")
+
+student1 = Student("Ann", "Architecture", 89)
+student2 = Student("Bob", "Business Foundations", 83)
+student3 = Student("Cathy", "Computer Science", 95)
+
+print(f"Students count: {Student.student_count}")
+student1.display_info()
+student2.display_info()
+student3.display_info()
