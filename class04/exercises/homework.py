6742ff7bead94706b60228fc8aa945288bdb827e
Jenea-G <eugeniagurulea@gmail.com>
2026-05-03 15:13:10 -0400

    add class vs instance attribute explanation

---
+++ class04/exercises/homework.py
@@ -0,0 +1,9 @@
+# Exercise 1 -- Identify attribute type
+
+class Book:
+    ... # class attribute
+
+    def __init__ (self, name, author):
+        self.name = name # instance attribute
+        self.author = author # instance attribute
+
--- class04/main.py
+++ class04/main.py
@@ -75,3 +75,51 @@ print(product2.price)
 
 print(product2.category)
 print(product1.category)
+
+# shadowing a class attribute
+
+class Employee:
+    bonus = 0.5
+
+    def __init__ (self, name):
+        self.name = name
+
+employee1 = Employee("John")
+employee2 = Employee("Jane")
+
+print(f"Employee 1 bounus: {employee1.bonus}")
+print(f"Employee 2 bounus: {employee2.bonus}")
+
+Employee.bonus = 1
+
+print(f"Employee 1 bounus: {employee1.bonus}")
+print(f"Employee 2 bounus: {employee2.bonus}")
+
+employee1.bonus = 2 # changing the shadow attribute
+                    # this doesnt change the class attribute itself!
+                    # we create a new instance attribute on employee1!
+
+employee3 = Employee("test")
+
+print(f"Employee 1 bounus: {employee1.bonus}")
+print(f"Employee 2 bounus: {employee2.bonus}")
+print(f"Employee 3 bounus: {employee3.bonus}")
+
+# case study
+
+# A good use case for class attributes are :
+    # shared across all instances
+    # conceptually the same for the whole class
+    # they're usually configuration-like or constant-like
+    # they're counters or class-wide metadata
+
+# Bad use cases for class attributes are :
+    # values that should usually be different per object.
+    # any value that changes oftn, or is indiidually different
+
+# Let's look at some examples:
+    # school name ---> class attribute
+    # max capacity ---> class attribute
+    # account password ---> instance attribute
+    # tax rates ---> class attribute
+    # course grade ---> instance attribute
