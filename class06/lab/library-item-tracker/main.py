5aaebb2489bb4a74c01fac96b51a81a5f7b86516
Jenea-G <eugeniagurulea@gmail.com>
2026-05-06 10:18:40 -0400

    add return_book method, rename display_book to display_info and correct punctuation in some print messages.

--- class06/lab/library-item-tracker/main.py
+++ class06/lab/library-item-tracker/main.py
@@ -4,23 +4,32 @@ class Book:
         self.author = author
         self.available = available
 
-    def display_book(self):
+    def display_info(self):
         if (self.available):
             availability = "available"
         else:
             availability = "not available"
-        print(f"The book {self.title} by {self.author} is {availability}.")
+        print(f"The book '{self.title}' by {self.author} is {availability}.")
 
     def borrow(self):
         if(self.available):
             self.available = False
-            print(f"The book {self.title} by {self.author} has been borrowed successfully")
+            print(f"The book '{self.title}' by {self.author} has been borrowed successfully.")
         else: 
-            print(f"The book {self.title} by {self.author} has already been borrowed. Try another one.")
+            print(f"The book '{self.title}' by {self.author} has already been borrowed. Try another one.")
+    
+    def return_book(self):
+        if(self.available == False):
+            self.available = True
+            print(f"The book '{self.title}' by {self.author} has been returned successfully.")
+        else: 
+            print(f"I already have '{self.title}' by {self.author}, this is not my copy.")
 
 book1 = Book("The Little Prince", "Antoine de Saint-Exupery", False)
 book2 = Book("1984", "George Orwell")
-book1.display_book()
-book2.display_book()
+book1.display_info()
+book2.display_info()
 
 book1.borrow()
+book1.return_book()
+book2.return_book()
