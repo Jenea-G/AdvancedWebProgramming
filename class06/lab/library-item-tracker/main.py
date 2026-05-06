e6090fd1e77608c44fd027c69b9f6c463da09aed
Jenea-G <eugeniagurulea@gmail.com>
2026-05-06 10:27:16 -0400

    add class attribute 'library name' and class  method 'change_library_name' and test it

--- class06/lab/library-item-tracker/main.py
+++ class06/lab/library-item-tracker/main.py
@@ -1,4 +1,6 @@
 class Book:
+    library_name = "Central Library"
+
     def __init__(self, title, author, available = True):
         self.title = title
         self.author = author
@@ -11,7 +13,19 @@ class Book:
             availability = "not available"
         print(f"The book {self.title} by {self.author} is {availability}.")
 
+    @classmethod
+    def change_library_name(cls, new_name):
+        cls.library_name = new_name
+
+    
+
+
 book1 = Book("The Little Prince", "Antoine de Saint-Exupery", False)
 book2 = Book("1984", "George Orwell")
 book1.display_book()
 book2.display_book()
+
+print("=== test library name change ===")
+print(Book.library_name)
+Book.change_library_name("Bibliotheque de Vieux Saint-Laurent")
+print("Library name changed to:", Book.library_name)
