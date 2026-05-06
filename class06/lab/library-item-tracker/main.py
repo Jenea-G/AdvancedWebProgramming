e97d35d0e570692954b85c73fe74b614b9d7a986
Jenea-G <eugeniagurulea@gmail.com>
2026-05-06 10:07:56 -0400

    created the branch feature/instance-methods and added borrow instance method

--- class06/lab/library-item-tracker/main.py
+++ class06/lab/library-item-tracker/main.py
@@ -11,7 +11,16 @@ class Book:
             availability = "not available"
         print(f"The book {self.title} by {self.author} is {availability}.")
 
+    def borrow(self):
+        if(self.available):
+            self.available = False
+            print(f"The book {self.title} by {self.author} has been borrowed successfully")
+        else: 
+            print(f"The book {self.title} by {self.author} has already been borrowed. Try another one.")
+
 book1 = Book("The Little Prince", "Antoine de Saint-Exupery", False)
 book2 = Book("1984", "George Orwell")
 book1.display_book()
 book2.display_book()
+
+book1.borrow()
