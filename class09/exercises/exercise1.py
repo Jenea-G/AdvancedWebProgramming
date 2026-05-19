02478ef35e74604af7259e5252169b53db99a634
Jenea-G <eugeniagurulea@gmail.com>
2026-05-19 14:38:18 -0400

    add ex1

---
+++ class09/exercises/exercise1.py
@@ -0,0 +1,91 @@
+# Ex.1
+
+# Create a class with:
+#   name
+#   private __gpa
+# Requirements:
+#   property gpa
+#   setter only accepts values between 0.0 and 4.0
+print("=== Ex.1 ===")
+class Student:
+    def __init__(self, name, gpa):
+        self.name = name
+        self.__gpa = gpa
+
+    @property
+    def gpa(self):
+        return self.__gpa
+    
+    @gpa.setter
+    def gpa(self, value):
+        if 0.0 < value <= 4:
+            self.__gpa = value
+        else: print("Provide a value between 0.0 and 4.0")
+
+s1 = Student("Sam", 3)
+print(f"{s1.name}'s gpa equals to {s1.gpa}")
+s1.gpa = 5
+s1.gpa = 3.8
+print(f"{s1.name}'s gpa equals to {s1.gpa}")
+
+
+# Ex.2
+
+# Create a class with:
+#   name
+#   internal _price
+# Requirements:
+#   property price
+
+# setter must reject negative values
+
+# Ex.3 
+
+# Create a class with:
+#   radius
+
+# Requirements:
+# a read-only property area
+
+# You should not store area directly; you should compute it.
+
+
+# Ex.4 
+
+# Create a class with:
+#   first_name
+#   last_name
+# Requirements:
+#   read-only property full_name
+
+# Ex.5
+
+# Create a class with:
+#   owner
+#   private __balance
+# Requirements:
+#   property balance
+#   setter prevents negative values
+#   method deposit(amount)
+#   method withdraw(amount)
+
+
+# Ex.6
+
+# Create a class with:
+#   name
+#   private __price
+#   quantity
+# Requirements:
+#   property price
+#   setter prevents negative values
+#   read-only property inventory_value
+
+# Ex.7
+
+# Create a class with:
+#   title
+#   private __rating
+# Requirements:
+#   property rating
+#   setter only accepts values between 0 and 10
