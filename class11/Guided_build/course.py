a7201918da7b6c173f999b10aa2be60cd1ddf651
Jenea-G <eugeniagurulea@gmail.com>
2026-05-26 12:14:34 -0400

    add methods that use enum

--- class11/Guided_build/course.py
+++ class11/Guided_build/course.py
@@ -11,3 +11,13 @@ class Course:
 
     def display_info(self):
         print(f"{self.title} | Capacity: {self.capacity} | Status: {self.status.value}")
+
+    def close_registration(self):
+        self.status = CourseStatus.CLOSED
+
+    def cancel_course(self):
+        self.status = CourseStatus.CANCELLED
+    
+    def reopen_course(self):
+        self.status = CourseStatus.OPEN
+
