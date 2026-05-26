1099e4dc299ae06bbabe997b7cfcdd4382fd8829
Jenea-G <eugeniagurulea@gmail.com>
2026-05-26 12:13:02 -0400

    add status validation

--- class11/Guided_build/course.py
+++ class11/Guided_build/course.py
@@ -2,6 +2,12 @@ from status import CourseStatus
 
 class Course:
     def __init__(self, title, capacity, status):
+        if not isinstance(status, CourseStatus):
+            raise ValueError("status must be a CourseStatus value")
+        
         self.title = title
         self.capacity = capacity
-        self.status = CourseStatus.OPEN
+        self.status = status
+
+    def display_info(self):
+        print(f"{self.title} | Capacity: {self.capacity} | Status: {self.status.value}")
