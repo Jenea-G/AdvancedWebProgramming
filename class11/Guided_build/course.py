7e50ed06c20693e59cf353329ebab980361a0fa3
Jenea-G <eugeniagurulea@gmail.com>
2026-05-26 12:27:42 -0400

    add and test status as property

--- class11/Guided_build/course.py
+++ class11/Guided_build/course.py
@@ -2,12 +2,21 @@ from status import CourseStatus
 
 class Course:
     def __init__(self, title, capacity, status):
-        if not isinstance(status, CourseStatus):
-            raise ValueError("status must be a CourseStatus value")
+
         
         self.title = title
         self.capacity = capacity
         self.status = status
+
+    @property
+    def status(self):
+        return self.__status
+
+    @status.setter
+    def status(self, value):
+        if not isinstance(value, CourseStatus):
+            raise ValueError("status must be a CourseStatus value")
+        self.__status = value
 
     def display_info(self):
         print(f"{self.title} | Capacity: {self.capacity} | Status: {self.status.value}")
--- class11/Guided_build/main.py
+++ class11/Guided_build/main.py
@@ -12,3 +12,15 @@ course1.display_info()
 
 course2.reopen_course()
 course2.display_info()
+
+try:
+    course3 = Course("Testing techinques", 20, "open")
+except ValueError as e:
+    print("Error:", e)
+
+try:
+    course3 = Course("Testing techinques - 2nd attempt", 20, CourseStatus.OPEN)
+except ValueError as e:
+    print("Error:", e)
+
+course3.display_info()
