970863323d314f1d17b0b998e1b926d3af530636
Jenea-G <eugeniagurulea@gmail.com>
2026-06-03 11:16:29 -0400

    add ex.2 and 3

--- class14/exercies/main.py
+++ class14/exercies/main.py
@@ -83,3 +83,84 @@ else:
     print(f"The withdrawal operation succeded. You have ${acc1.balance} left on your account")
 # finally:
 #     print("Operation completed")
+
+# 2
+print()
+print("====== Exercise 2 =====")
+
+# Create a class with:
+# property Celcius
+
+# Raise an exception if:
+# temperature is below absolute zero
+class Temperature:
+    def __init__(self, temp):
+        self.temperature = temp
+    
+    @property
+    def temperature(self):
+        return self.__temp
+    
+    @temperature.setter
+    def temperature(self, value):
+        if -273.15 > value:
+            raise ValueError("Temperature cannot be below absolute zero")
+        self.__temp = value
+
+try:
+    t1 = Temperature(-300)
+except ValueError as error:
+    print(error)
+else:
+    print(f"Temperature is {t1.temperature}")
+
+# 3
+print()
+print("====== Exercise 3 =====")
+# Create:
+# Class NegativePriceError(Exception):
+# pass
+
+# Then use it in a product class.
+
+class NegativePriceError(Exception):
+    pass
+
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    @property
+    def price(self):
+        return self.__price
+    
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise NegativePriceError("The price cannot be negative")
+        self.__price = value
+
+    def setPrice(self, value):
+        self.price = value
+
+try:
+    p1 = Product("Mouse", -5)
+except NegativePriceError as error:
+    print(error)
+else:
+    print(f"The product {p1.name} was successfully added")
+
+try:
+    p2 = Product("Headset", 55)
+except NegativePriceError as error:
+    print(error)
+else:
+    print(f"The product {p2.name} was successfully added")
+
+try:
+    p2.setPrice(60)
+except NegativePriceError as error:
+    print(error)
+else:
+    print(f"The price of {p2.name} was successfully updated and is now {p2.price}$")
