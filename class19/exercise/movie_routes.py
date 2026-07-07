5a9ba5aad00168768b2ca374dde1c18692859e74
Jenea-G <eugeniagurulea@gmail.com>
2026-07-07 11:58:22 -0400

    add search movies by genre

--- class19/exercise/movie_routes.py
+++ class19/exercise/movie_routes.py
@@ -18,3 +18,19 @@ def movies_list():
 def movie_detail():
     movie_title = request.args.get("title", "Unknown Movie")
     return render_template("movie.html", movie_title = movie_title)
+
+@movies.route("/search")
+def search():
+    genre = request.args.get("genre", "")
+
+    if genre:
+        filtered = []
+
+        for movie in MOVIES:
+            if movie["genre"] == genre:
+                filtered.append(movie)
+
+    else:
+        filtered = MOVIES
+
+    return render_template("search.html", movies = filtered, genre = genre)
