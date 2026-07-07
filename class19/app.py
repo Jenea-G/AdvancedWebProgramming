54d992058b08917ed5ffd92b262f24d09204ee49
Jenea-G <eugeniagurulea@gmail.com>
2026-07-07 09:38:33 -0400

    class19 flask refresh

---
+++ class19/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Hello!<h1>" #render on the browser
+
+@app.route("/greet")
+def greet():
+    # get arguments from the URL
+    # such as /greet/?name=Joe
+    name = request.args.get("name", "Guest")
+    return f"<h1>Hello, {name}!</h1>"
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About me")
+
+# the routes are our backend entry points!
+# templates are redered by Flask!
+# query parameters let the broweser send lightweight input!
+
+# 
