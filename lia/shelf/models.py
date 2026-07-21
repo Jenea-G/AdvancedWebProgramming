8c62b49e86f1a81f1ec1902941f8f9fe7e2b488e
Jenea-G <eugeniagurulea@gmail.com>
2026-07-21 14:45:04 -0400

    create User model

--- lia/shelf/models.py
+++ lia/shelf/models.py
@@ -4,3 +4,24 @@ from werkzeug.security import (check_password_hash, generate_password_hash)
 
 # starting db
 db = SQLAlchemy()
+
+# create User model
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    
+    password_hash = db.Column(db.String(255), nullable=False)
+    
+    # methods
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    # log
+    def __repr__(self):
+        return(f"User {self.id}: {self.username}")
