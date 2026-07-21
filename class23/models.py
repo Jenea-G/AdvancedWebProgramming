19e7090e621fcedde0121d2ab6d9c2130492814d
Jenea-G <eugeniagurulea@gmail.com>
2026-07-21 10:34:10 -0400

    add class23 password auth. app

---
+++ class23/models.py
@@ -0,0 +1,30 @@
+from flask_login import UserMixin
+
+from flask_sqlalchemy import SQLAlchemy
+
+from werkzeug.security import (check_password_hash, generate_password_hash)
+
+# let's start our db
+db = SQLAlchemy()
+
+
+# creating the model class
+class Member(UserMixin, db.Model):
+    __tablename__ = "memeber"
+
+    # defining the fields
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable = False, unique = True)
+    email = db.Column(db.String(255), nullable = False, unique = True)
+    password_hash = db.Column(db.String(255), nullable = False)
+
+    # methods
+    def set_passwor(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    #log
+    def __repr__(self):
+        return(f"<Member {self.id}: {self.username}")
